03d9fb78a4f3df73ef0c0d8a2ba4b7524154a5f8
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-19 14:28:05 -0300

    checkpoint 49

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -32,11 +32,22 @@ class App extends Component {
 	}
 
 	render() {
+		const style = {
+			backgroundColor: 'white',
+			font: 'inherit',
+			border: '1px solid blue',
+			padding: '8px',
+			cursor: 'pointer'
+		}	
+
+
 		return (
 			<div className="App">
 				<h1>Hi, I'm a React App</h1>
 				<p>This is really working!</p>
-				<button onClick={() => this.switchNameHandler('Aline')}>Switch Name</button>
+				<button 
+					style={style}
+					onClick={() => this.switchNameHandler('Aline')}>Switch Name</button>
 				<Person
 					name={this.state.persons[0].name}
 					age={this.state.persons[0].age}/>
