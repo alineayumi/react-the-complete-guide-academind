b66be8994d8889ef8840b71b0b3f78c52f17cf2a
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-19 13:51:00 -0300

    checkpoint: 45

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-      </div>
-    );
-  }
+	state = {
+		persons: [
+			{ name: 'Max', age: 28 },
+			{ name: 'Manu', age: 29 },
+			{ name: 'Stephanie', age: 26 }
+		]
+	}
+
+	switchNameHandler = () => {
+		// console.log('Was clicked!');
+		// Don't do this: this.state.persons[0].name = 'Aline';
+		this.setState({persons: [
+			{ name: 'Aline', age: 28 },
+			{ name: 'Lucia', age: 29 },
+			{ name: 'Toshiro', age: 26 }
+		]});
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<h1>Hi, I'm a React App</h1>
+				<p>This is really working!</p>
+				<button onClick={this.switchNameHandler}>Switch Name</button>
+				<Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+				<Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+				<Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+			</div>
+		);
+		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
+	}
 }
 
 export default App;
