5100b89d6faacc7ba7b7d0474bd899c30f2ee19f
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-20 10:34:08 -0300

    checkpoint 53

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
 			{ name: 'Max', age: 28 },
 			{ name: 'Manu', age: 29 },
 			{ name: 'Stephanie', age: 26 }
-		]
+		],
+		otherState: 'some other value',
+		showPersons: false
 	}
 
 	switchNameHandler = (newName) => {
@@ -31,6 +33,11 @@ class App extends Component {
 		})
 	}
 
+	togglePersonsHandler = () => {
+		const doesShow = this.state.showPersons;
+		this.setState({showPersons: !doesShow});
+	}
+
 	render() {
 		const style = {
 			backgroundColor: 'white',
@@ -47,18 +54,23 @@ class App extends Component {
 				<p>This is really working!</p>
 				<button 
 					style={style}
-					onClick={() => this.switchNameHandler('Aline')}>Switch Name</button>
-				<Person
-					name={this.state.persons[0].name}
-					age={this.state.persons[0].age}/>
-				<Person
-					name={this.state.persons[1].name}
-					age={this.state.persons[1].age}
-					click={this.switchNameHandler.bind(this, 'Nicholas')}
-					changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
-				<Person 
-				name={this.state.persons[2].name} 
-				age={this.state.persons[2].age} />
+					onClick={this.togglePersonsHandler}>Switch Name</button>
+				{
+					this.state.showPersons ?
+						<div>
+							<Person
+								name={this.state.persons[0].name}
+								age={this.state.persons[0].age} />
+							<Person
+								name={this.state.persons[1].name}
+								age={this.state.persons[1].age}
+								click={this.switchNameHandler.bind(this, 'Nicholas')}
+								changed={this.nameChangedHandler}>My Hobbies: Racing</Person>
+							<Person
+								name={this.state.persons[2].name}
+								age={this.state.persons[2].age} />
+						</div> : null
+				}
 			</div>
 		);
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
