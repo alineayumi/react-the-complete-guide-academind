0ba0dcf80bdc46038169fa6298c3f464e61cfc6c
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-20 22:54:43 -0300

    67: dinamically assign style and classes

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -42,7 +42,8 @@ class App extends Component {
 
 	render() {
 		const style = {
-			backgroundColor: 'white',
+			backgroundColor: 'green',
+			color: 'white',
 			font: 'inherit',
 			border: '1px solid blue',
 			padding: '8px',
@@ -64,12 +65,21 @@ class App extends Component {
 					})}
 				</div>
 			);
+			style.backgroundColor = 'red';
+		}
+
+		let classes = [];
+		if (this.state.persons.length <= 2) {
+			classes.push('red');
+		}
+		if (this.state.persons.length <= 1) {
+			classes.push('bold');
 		}
 
 		return (
 			<div className="App">
 				<h1>Hi, I'm a React App</h1>
-				<p>This is really working!</p>
+				<p className={classes.join(' ')}>This is really working!</p>
 				<button
 					style={style}
 					onClick={this.togglePersonsHandler}>Toggle Persons</button>
