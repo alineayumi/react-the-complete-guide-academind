5fb7cb4f1c395ccb7519dfb3d321ae22de186aee
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-20 23:02:19 -0300

    68: adding and using radium to style js

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
+import Radium from 'radium';
 
 class App extends Component {
 	state = {
@@ -47,7 +48,11 @@ class App extends Component {
 			font: 'inherit',
 			border: '1px solid blue',
 			padding: '8px',
-			cursor: 'pointer'
+			cursor: 'pointer',
+			':hover': {
+				backgroundColor: 'lightgreen',
+				color: 'black'
+			}
 		}
 
 		let persons = null;
@@ -66,6 +71,10 @@ class App extends Component {
 				</div>
 			);
 			style.backgroundColor = 'red';
+			style[':hover'] = {
+				backgroundColor: 'salmon',
+				color: 'black'
+			}
 		}
 
 		let classes = [];
@@ -90,4 +99,4 @@ class App extends Component {
 	}
 }
 
-export default App;
+export default Radium(App);
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Radium from 'radium';
 import './Person.css';
 
 const person = (props) => {
@@ -12,4 +13,4 @@ const person = (props) => {
 	);
 };
 
-export default person
+export default Radium(person);
