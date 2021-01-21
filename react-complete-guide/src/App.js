53d8aad8ffd05578dad0b0d965b7de6aa642b688
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-20 23:48:41 -0300

    73: CSS modules before eject

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
-import styled from 'styled-components';
-
-const StyledButton = styled.button`
-	background-color: ${props => props.alt ? 'red' : 'green'};
-	color: white;
-	font: inherit;
-	border: 1px solid blue;
-	padding: 8px;
-	cursor: pointer;
-
-	&:hover {
-		background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-		color: black
-	}
-`;
 class App extends Component {
 	state = {
 		persons: [
@@ -85,7 +70,7 @@ class App extends Component {
 			<div className="App">
 				<h1>Hi, I'm a React App</h1>
 				<p className={classes.join(' ')}>This is really working!</p>
-				<StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>Toggle Persons</StyledButton>
+				<button onClick={this.togglePersonsHandler}>Toggle Persons</button>
 				{persons}
 			</div>
 		);
