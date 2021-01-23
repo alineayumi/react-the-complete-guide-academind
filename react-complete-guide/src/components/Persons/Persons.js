b63f71ddada5f67cb33a3dcc4739f972563d4b60
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-23 17:08:21 -0300

    100: purecomponent instead shouldcomponentupdate

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -14,7 +14,9 @@ class Persons extends Component {
 
 	shouldComponentUpdate(nextProps, nextState) {
 		console.log('[Persons.js] shouldComponentUpdate');
-		if (nextProps.persons !== this.props.persons) {
+		if (nextProps.persons !== this.props.persons ||
+			nextProps.changed !== this.props.changed ||
+			nextProps.clicked !== this.props.clicked) {
 			return true;
 		} else {
 			return false;
