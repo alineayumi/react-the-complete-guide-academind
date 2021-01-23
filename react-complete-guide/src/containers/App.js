cc7bf45698e5f09f1c3bec4395a7efccc2581304
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-23 17:48:19 -0300

    106: Higher order components intro

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
+import WithClass from '../hoc/WithClass';
 
 class App extends Component {
 	constructor(props) {
@@ -78,7 +79,7 @@ class App extends Component {
 		}
 
 		return (
-			<div className={classes.App}>
+			<WithClass classes={classes.App}>
 				<button onClick={() => { this.setState({ showCockpit: false }); }}>Remove Cockpit</button>
 				{this.state.showCockpit === true ? <Cockpit
 					title={this.props.appTitle}
@@ -86,7 +87,7 @@ class App extends Component {
 					personsLength={this.state.persons.length}
 					onClick={this.togglePersonsHandler} /> : null}
 				{persons}
-			</div>
+			</WithClass>
 		);
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!!!'));
 	}
