b91939efc675fce029fb3a18a478a8c669f5883f
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-23 14:16:57 -0300

    91: component update lifecycle (for props changes)

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -25,10 +25,6 @@ class App extends Component {
 		return state;
 	}
 
-	componentWillMount() {
-		console.log('[App.js] componentWillMount');
-	}
-
 	componentDidMount() {
 		console.log('[App.js] componentDidMount');
 	}
