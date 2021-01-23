70210fca35a024930f407455f5e380951be58436
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-23 18:27:19 -0300

    108: setting state correctly

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classes from './Person.css';
 import Aux from '../../../hoc/Aux';
-
+import withClass from '../../../hoc/withClass';
 class Person extends Component {
 
 	render() {
@@ -18,4 +18,4 @@ class Person extends Component {
 	}
 };
 
-export default Person;
+export default withClass(Person, classes.Person);
