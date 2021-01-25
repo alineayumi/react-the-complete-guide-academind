75446453e6846944f6bceaf74b91dc682cc40c1e
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-25 12:38:22 -0300

    calculating the ingredients length and displaying message if there is no ingr yet

--- the-burger-builder/src/components/Burger/Burger.js
+++ the-burger-builder/src/components/Burger/Burger.js
@@ -3,13 +3,18 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  const transformed_ingredients = Object.keys(props.ingredients).map(
-    (igKey) => {
+  let transformed_ingredients = Object.keys(props.ingredients)
+    .map((igKey) => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />;
       });
-    }
-  );
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformed_ingredients.length === 0) {
+    transformed_ingredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
