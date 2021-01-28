d27d75ae2252e3322102a4f5a57644e90ac49364
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-28 19:02:32 -0300

    132: outputting multiple build controls

--- the-burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ the-burger-builder/src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -3,7 +3,7 @@ import classes from './BuildControl.module.css';
 
 const buildControl = (props) => (
 	<div className={classes.BuildControl}>
-		<div className={classes.Label}>props.label</div>
+		<div className={classes.Label}>{props.label}</div>
 		<button className={classes.Less}>Less</button>
 		<button className={classes.More}>More</button>
 	</div>
--- the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ the-burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 class BurgerBuilder extends Component {
   state = {
@@ -15,10 +16,8 @@ class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-        <div>
-          <Burger ingredients={this.state.ingredients} />
-        </div>
-        <div>Build Controls</div>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </Aux>
     );
   }
