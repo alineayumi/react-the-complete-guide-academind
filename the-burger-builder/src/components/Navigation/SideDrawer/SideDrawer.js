267c8eb8c1880e0130b6846456adf26753e53ea5
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-01-31 17:54:16 -0300

    148: removing nav toolbar from mobile devices

--- the-burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
+++ the-burger-builder/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import Logo from '../../Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
-import classes from './SideDrawer.module.css';
-
+import React from "react";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import classes from "./SideDrawer.module.css";
 
 const sideDrawer = (props) => {
-	//...
-	return (
-		<div className={classes.SideDrawer}>
-			<Logo height="11%"/>
-			<nav>
-				<NavigationItems/>
-			</nav>
-		</div>
-	);
+  //...
+  return (
+    <div className={classes.SideDrawer}>
+      <div className={classes.Logo}>
+        <Logo />
+      </div>
+
+      <nav>
+        <NavigationItems />
+      </nav>
+    </div>
+  );
 };
 
 export default sideDrawer;
--- the-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ the-burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import classes from './Toolbar.module.css';
-import Logo from '../../../components/Logo/Logo';
-import NavigationItems from '../NavigationItems/NavigationItems';
+import React from "react";
+import classes from "./Toolbar.module.css";
+import Logo from "../../../components/Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 
 const toolbar = (props) => (
-	<header className={classes.Toolbar}>
-		<div>MENU</div>
-		<Logo height="80%"/>
-		<nav>
-			<NavigationItems/>
-		</nav>
-	</header>
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
+  </header>
 );
 
 export default toolbar;
