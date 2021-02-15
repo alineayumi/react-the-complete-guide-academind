3e4f5891c3e10e608b96b62243745169a6c85db6
Aline Ayumi Fukuhara <alineayumi@gmail.com>
2021-02-15 17:44:00 -0300

    section 9: checkpoint finished

--- http-ajax/src/components/FullPost/FullPost.js
+++ http-ajax/src/components/FullPost/FullPost.js
@@ -1,22 +1,54 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from '../../axios';
 
-import './FullPost.css';
+// import axios from "axios";
+import "./FullPost.css";
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+  state = {
+    loadedPost: null,
+  };
 
-        );
-        return post;
+  componentDidUpdate() {
+    if (this.props.id) {
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+      ) {
+        axios
+          .get("/posts/" + this.props.id)
+          .then((response) => {
+            this.setState({ loadedPost: response.data });
+          });
+      }
     }
+  }
+
+  deletePostMethod = () => {
+	  axios.delete('/posts/' + this.props.id)
+	  	.then(response => {
+			console.log(response);
+		  });
+  }
+
+  render() {
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = <p style={{ textAlign: "center" }}>Loading...</p>;
+    }
+    if (this.state.loadedPost) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.loadedPost.title}</h1>
+          <p>{this.state.loadedPost.body}</p>
+          <div className="Edit">
+            <button className="Delete" onClick={this.deletePostMethod}>Delete</button>
+          </div>
+        </div>
+      );
+    }
+    return post;
+  }
 }
 
 export default FullPost;
--- http-ajax/src/components/NewPost/NewPost.js
+++ http-ajax/src/components/NewPost/NewPost.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+// import axios from 'axios';
+import axios from '../../axios';
 
 import './NewPost.css';
 
@@ -8,6 +10,18 @@ class NewPost extends Component {
         content: '',
         author: 'Max'
     }
+
+	postDataHandler = () => {
+		const post = {
+			title: this.state.title,
+			body: this.state.body,
+			author: this.state.author,
+		}
+		axios.post('/posts', post)
+			.then(response => {
+				console.log(response);
+			});
+	}
 
     render () {
         return (
@@ -22,7 +36,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
